062bed8a9c9875b89d92c2e6964761449fc10c55
Layo Folaranmi <thedarkkid.codes@gmail.com>
2020-11-18 10:47:37 +0100

    changed eaxcess.json to messenger.json to fit template profile

--- src/helpers/Config.ts
+++ src/helpers/Config.ts
@@ -1,7 +1,7 @@
 import Files, { getFileContents, projectBaseDir } from "./Files";
 
 export default class Config{
-  private static file = JSON.parse(getFileContents(projectBaseDir("eaxcess.json")));
+  private static file = JSON.parse(getFileContents(projectBaseDir("messenger.json")));
   static folders: any = Config.file.messengerOptions.messagesFolders;
   static messengerFilePath = Config.file.messengerOptions.messengerPath;
 }
